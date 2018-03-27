a4c71c94233bc2d0c7be11443441b5d8870cc325
Eric Razor <eric.razorjr@gmail.com>
2018-03-27 01:15:39 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,16 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+
+
+function addElementToBeginningOfArray(array,element){
+  var arr = array.push(array[element])
+  return 
+}
+
+/*
+
+function accessElementInArray(array,index){
+  array = ["tokyo","russia","pairs","france"];
+  index = array[index];
+  return index
+}
+*/
